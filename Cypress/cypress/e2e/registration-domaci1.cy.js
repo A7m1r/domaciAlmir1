ce76ba2306bfe9229f49cf3384569de702b851bf
Almir Durakovic <almir.durakovic84@gmail.com>
2022-12-15 16:19:22 +0100

    Update registration-domaci1.cy.js

--- Cypress/cypress/e2e/registration-domaci1.cy.js
+++ Cypress/cypress/e2e/registration-domaci1.cy.js
@@ -1,60 +1,15 @@
-const locators = require("../fixtures/locators.json")
-const {faker} = require('@faker-js/faker')
-var userPass = faker.internet.password ()
-let sviNum = faker.random.numeric (15)
-describe ('Register testing', () => {
-    it('Visit Gallery', () => {
-        cy.visit('/')
-    })
-    it('Go to Register', () => {
-        cy.visit("/")
-        cy.get(locators.registration.regBtn).eq(2).click()
-    })
-    it('Register', () => {
-        cy.visit('/')
-        cy.get(locators.registration.regBtn).eq(2).click()
-        cy.get(locators.registration.firstName).type(faker.name.firstName())
-        cy.get(locators.registration.lastName).type(faker.name.lastName())
-        cy.get(locators.registration.email).type(faker.internet.email())
-        cy.get(locators.registration.password).type(userPass)
-        cy.get(locators.registration.passwordConfirmation).type(userPass)
-        cy.get(locators.registration.acceptCheckBox).click()
+describe ("Registration test",() => {
+    it ('Registration',()=> {
+        cy.visit ("/")
+        cy.get(':nth-child(2) > .nav-link').click ()
+        cy.get('#first-name').type ("Almir")
+        cy.get('#last-name').type ("Tester")
+        cy.get('#email').type("almirtester@mail.com");
+        cy.get('#password').type ("Almir1234")
+        cy.get('#password-confirmation').type ("Almir1234")
+        cy.get('.form-check-input').click()
         cy.get('.btn').click()
     })
-    it ("First name in numbers", () => {
-        cy.visit('/')
-        cy.get(locators.registration.regBtn).eq(2).click()
-        cy.get(locators.registration.firstName).type(faker.random.numeric(9))
-        cy.get(locators.registration.lastName).type(faker.name.lastName())
-        cy.get(locators.registration.email).type(faker.internet.email())
-        cy.get(locators.registration.password).type(userPass)
-        cy.get(locators.registration.passwordConfirmation).type(userPass)
-        cy.get(locators.registration.acceptCheckBox).click()
-        cy.get('.btn').click()
-    })
-    it ("Password all numbers", () => {
-        cy.visit('/')
-        cy.get(locators.registration.regBtn).eq(2).click()
-        cy.get(locators.registration.firstName).type(faker.name.firstName())
-        cy.get(locators.registration.lastName).type(faker.name.lastName())
-        cy.get(locators.registration.email).type(faker.internet.email())
-        cy.get(locators.registration.password).type(sviNum)
-        cy.get(locators.registration.passwordConfirmation).type(sviNum)
-        cy.get(locators.registration.acceptCheckBox).click()
-        cy.get('.btn').click()
-    })
-    it ("Last name in letters and symbols", () => {
-        cy.visit('/')
-        cy.get(locators.registration.regBtn).eq(2).click()
-        cy.get(locators.registration.firstName).type(faker.name.firstName ())
-        cy.get(locators.registration.lastName).type(faker.finance.iban ())
-        cy.get(locators.registration.email).type(faker.internet.email())
-        cy.get(locators.registration.password).type(userPass)
-        cy.get(locators.registration.passwordConfirmation).type(userPass)
-        cy.get(locators.registration.acceptCheckBox).click()
-        cy.get('.btn').click()
-    })
-})
-afterEach(() => {
-    cy.clearCookies()
+    
+
 })
